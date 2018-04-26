2a08d25f765dee61a4b3d2082f81c692e9b7fe10
amadogr50 <amadogr50@gmail.com>
2018-04-26 07:42:38 -0500

    Turn variable starts in 1 now.

--- init.h
+++ init.h
@@ -7,20 +7,20 @@
 #include "board.h"
 #include "moves.h"
 
-void readMoves(coordinate moveFrom, coordinate moveTo, char move[5], int *validMove, piece board[8][8]){
-	//
+void readMoves(int turnPlayer, coordinate moveFrom, coordinate moveTo, char move[5], int *validMove, piece board[8][8]){
+	//Function to determinate 
 	moveFrom.x = changeAsciiToDecimal(move[0]);
 	moveFrom.y = changeAsciiToDecimal(move[1]);
 	moveTo.x =  changeAsciiToDecimal(move[2]);
 	moveTo.y =  changeAsciiToDecimal(move[3]);
-	*validMove = toMove(moveFrom, moveTo, board);
+	*validMove = toMove(turnPlayer, moveFrom, moveTo, board);
 }
 
 void init_game() {
 	//Creates the board and displays it for the first time
 	piece board[8][8];
 	generate_board(board);
-	int turn = 0;
+	int turn = 1;
 	//Variables for storing move info
 	char move[5];
 	coordinate moveTo;
@@ -31,23 +31,23 @@ void init_game() {
 		system("cls");
 		display_board(board);
 		validMove = 0;
-		if (turn == 0){
+		if (turn == 1){
 			//Get the movement instructions if it is a not valid move, it repeats the code until getting a valid move
 			while (validMove == 0) {
 				//Sintax for making a move: <x1><y1><x2><y2> o <moveFrom.x><moveFrom.y><moveTo.x><moveTo.y>
 				printf("Mueven blancas:\n");
 				scanf("%s", move);
-				readMoves(moveFrom, moveTo, move, &validMove, board);
+				readMoves(turn, moveFrom, moveTo, move, &validMove, board);
 			}
 			//Change turn to 1
 			turn++;
-		} else if (turn == 1) {
+		} else if (turn == 2) {
 			//Get the movement instructions if it is a not valid move, it repeats the code until getting a valid move
 			while(validMove == 0) {
 				//Sintax for making a move: <x1><y1><x2><y2> o <moveFrom.x><moveFrom.y><moveTo.x><moveTo.y>
 				printf("Mueven negras:\n");
 				scanf("%s", move);
-				readMoves(moveFrom, moveTo, move, &validMove, board);
+				readMoves(turn, moveFrom, moveTo, move, &validMove, board);
 			}
 			//Change turn to 0
 			turn--;
