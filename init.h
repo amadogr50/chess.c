738eea609c5fce6390b9086bc73d5bb8a053d1d1
amadogr50 <amadogr50@gmail.com>
2018-05-01 19:49:21 -0500

    Turns order fixed

--- init.h
+++ init.h
@@ -20,7 +20,7 @@ void init_game() {
 	//Creates the board and displays it for the first time
 	piece board[8][8];
 	generate_board(board);
-	int turn = 1;
+	int turn = 2;
 	//Variables for storing move info
 	char move[5];
 	coordinate moveTo;
@@ -28,10 +28,11 @@ void init_game() {
 	//Game bucle
 	int validMove;
 	while (validateCheckmate(board) == 0) {
+		system("pause");
 		system("cls");
 		display_board(board);
 		validMove = 0;
-		if (turn == 1){
+		if (turn == 2){
 			//Get the movement instructions if it is a not valid move, it repeats the code until getting a valid move
 			while (validMove == 0) {
 				//Sintax for making a move: <x1><y1><x2><y2> o <moveFrom.x><moveFrom.y><moveTo.x><moveTo.y>
@@ -41,7 +42,7 @@ void init_game() {
 			}
 			//Change turn to 1
 			turn++;
-		} else if (turn == 2) {
+		} else if (turn == 1) {
 			//Get the movement instructions if it is a not valid move, it repeats the code until getting a valid move
 			while(validMove == 0) {
 				//Sintax for making a move: <x1><y1><x2><y2> o <moveFrom.x><moveFrom.y><moveTo.x><moveTo.y>
