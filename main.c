8ae4bc2de7f528d12b8c09019b311fb175b30d73
Omar Cano <omartian21@gmail.com>
2018-05-07 10:12:31 -0500

    added multi.h library

--- main.c
+++ main.c
@@ -6,17 +6,25 @@
 #include "init.h"
 #include "instructions.h"
 #include "scores.h"
+#include "mutli.h"
 
 // Opciones
 #define new 1
-#define scores 2
-#define instructions 3
+#define multiplayer 2
+#define scores 3
+#define instructions 4
 #define exit 0
 
 int main(void) {
 	int init;
     checkInit:
-	printf("WELCOME TO CHESS PE2018v0.01\n\nType one of the following:\n\t1: To start a new game\n\t2: To show scores\n\t3: To show instructions\n\t0: To exit game\n\n");
+	printf("WELCOME TO CHESS PE2018v0.01\n
+				\nType one of the following:\n
+				\t1: To start a new game\n
+				\t2: To start a multiplayer game\n
+				\t3: To show the scoreboard\n
+				\t4: To show the instructions\n
+				\t0: To exit the game\n");
 	scanf("%d", &init);
 	switch (init) {
 	case new:
@@ -24,6 +32,10 @@ int main(void) {
 		printf("New game\n");
 		init_game();
 		break;
+	case multiplayer:
+		system("cls");
+		printf("Multiplayer\n");
+		init_multi();
 	case scores:
 		system("cls");
 		printf("Scores\n");
---
+++ multi.h
@@ -0,0 +1,11 @@
+
+int multiselection;
+printf("\n 1. To host a game.
+        \n 2. To join a game.");
+scanf("%d", &multiselection);
+if(multiselection==1)
+  init_server();
+else
+  init_client();
+
+        
